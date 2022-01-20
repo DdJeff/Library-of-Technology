2931fe236358143c184e5da031b9da150b262c1e
DEONTAY JEFFERSON <djefferson46@gmail.com>
2022-01-19 18:39:49 -0800

    Added the Book Model

---
+++ Library of Technology/Models/Books.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library_of_Technology.Models
+{
+    public class Books
+    {
+
+        public int Id { get; set; }
+
+        public string Title{ get; set; }
+
+        public string Author { get; set; }
+
+        public int Rating { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Image { get; set; }
+
+        public double Cost { get; set; }
+
+        public string Published { get; set; }
+    }
+}
